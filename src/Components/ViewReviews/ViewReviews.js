1fffd047ac97c41b1aeeab849d195bc235a3dbc0
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-04-03 11:28:24 -0700

    Added ReviewService, added some code to ViewReviews Component

--- src/Components/ViewReviews/ViewReviews.js
+++ src/Components/ViewReviews/ViewReviews.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReviewList from "../ReviewList/ReviewList"
+
 
 function ViewReviews(props) {
+    const [reviews, setReviews] = useState([])
+    const [currentMovie, setCurrentMovie] = useState([])
     
+
+    useEffect(()=> { 
+        getReviews()
+    })
+
+
     return (
         <div>
-            
+            <ReviewList reviews={props.reviews} like={props.like} dislike={props.dislike}/>
         </div>
     )
 }
---
+++ src/Services/ReviewService.js
@@ -0,0 +1,23 @@
+export async function getReviews()  {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}user/reviews`,   {
+        method: "GET",
+        headers: { "Content-Type": "application/json"}
+    }).then((response) => response.json())
+}
+
+export async function addReview(newReview)  {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}user/reviews`, {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(newReview)
+    }).then((response) => response.json())
+}
+
+export async function deleteReview(id)  {
+    return(`${process.env.REACT_APP_API_BASE_URL}admin/reviews`,{
+        method: "DELETE",
+        headers: {"Content-Type" : "applicaiton/json"},
+        body: JSON.stringify({"id" : id})
+    }).then((respons) => response.json())
+}
+
