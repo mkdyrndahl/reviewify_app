ba8ae8e3534e5c337d855e7f83d4770ec5e90142
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-04-03 11:41:10 -0700

    Added more like and dislike POSTing to ReviewService

--- src/Components/ViewReviews/ViewReviews.js
+++ src/Components/ViewReviews/ViewReviews.js
@@ -1,20 +1,29 @@
 import React, { useEffect } from "react";
 import ReviewList from "../ReviewList/ReviewList"
+import {
+    getReviews,
+    addReview,
+    deleteReview,
+    likeReview,
+    dislikeReview
+} from '../../Services/ReviewService'
 
-
-function ViewReviews(props) {
+function ViewReviews() {
     const [reviews, setReviews] = useState([])
     const [currentMovie, setCurrentMovie] = useState([])
-    
+
 
     useEffect(()=> { 
-        getReviews()
-    })
+        getReviews().then((json) => {
+            setReviews(json)
+        })
+    }, [])
+
 
 
     return (
         <div>
-            <ReviewList reviews={props.reviews} like={props.like} dislike={props.dislike}/>
+            <ReviewList reviews={reviews} like={props.like} dislike={props.dislike} delete={props.delete}/>
         </div>
     )
 }
--- src/Services/ReviewService.js
+++ src/Services/ReviewService.js
@@ -13,11 +13,27 @@ export async function addReview(newReview)  {
     }).then((response) => response.json())
 }
 
+export async function likeReview(newLike)  {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}user/reviews`, {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(newLike)
+    }).then((response) => response.json())
+}
+
+export async function dislikeReview(newDislike)  {
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}user/reviews`, {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(newDislike)
+    }).then((response) => response.json())
+}
+
 export async function deleteReview(id)  {
     return(`${process.env.REACT_APP_API_BASE_URL}admin/reviews`,{
         method: "DELETE",
         headers: {"Content-Type" : "applicaiton/json"},
         body: JSON.stringify({"id" : id})
-    }).then((respons) => response.json())
+    }).then((response) => response.json())
 }
 
