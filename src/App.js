a78f41efb4c4c28cf7e982b23ff3476e837c399e
Ryan Tran <tranp6@student.douglascollege.ca>
2021-04-05 13:08:28 -0700

    Added router, route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ManageMovie from './Components/ManageMovies/ManageMovies';
+import ViewReviews from './Components/ViewReviews/ViewReviews';
 
 function App() {
     return (
         <div className="App">
-            <ManageMovie />
+            <Router>
+                <Switch>
+                    <Route path="/admin" exact component={ManageMovie}/>
+                    <Route path="/review" exact component={ViewReviews}/>
+                    <Route path="/" exact component={ViewReviews}/>
+                </Switch>
+            </Router>
+
         </div>
     );
 }
